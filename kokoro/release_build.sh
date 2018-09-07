0a91ea866e3711036900e0c4216dcf288af33c53
Elliotte Harold <elharo@google.com>
2018-09-07 13:12:27 -0400

    don't need Cloud SDK

--- kokoro/release_build.sh
+++ kokoro/release_build.sh
@@ -5,9 +5,6 @@ set -e
 # Display commands to stderr.
 set -x
 
-sudo /opt/google-cloud-sdk/bin/gcloud components update
-sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
-
 cd github/plugins-login-common
 ./mvnw -Prelease -B -U verify
 
