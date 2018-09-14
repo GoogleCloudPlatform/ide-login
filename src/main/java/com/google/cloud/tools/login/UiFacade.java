4d955e6cdb00b58088ec715724e065c42dc354e1
Elliotte Harold <elharo@google.com>
2018-09-14 12:04:11 -0400

    Apache License

--- src/main/java/com/google/cloud/tools/login/UiFacade.java
+++ src/main/java/com/google/cloud/tools/login/UiFacade.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright 2018 Google LLC.
+ * Copyright 2014s Google LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
